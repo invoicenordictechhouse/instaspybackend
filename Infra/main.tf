1a12f619a45f685e3498755f14c58bffa55d09b6
Hussein Chukair <hussein.chukair@gmail.com>
2024-09-26 10:20:26 +0200

    Added variables and created different workspaces, dev and prod

--- Infra/main.tf
+++ Infra/main.tf
@@ -8,12 +8,26 @@ terraform {
 }
 
 provider "google" {
-  project                        = "annular-net-436607-t0"
-  region                         = var.region
-  impersonate_service_account    = "terraform-sa@annular-net-436607-t0.iam.gserviceaccount.com"
+  project                     = var.project_id
+  region                      = var.region
+  impersonate_service_account = var.service_account
 }
 
 resource "google_compute_network" "vpc_network" {
-    name = "terraform-network"
+  name = "terraform-network"
+}
+
+resource "google_bigquery_dataset" "login_dataset" {
+  dataset_id = "instaspy_login_dataset"
+  project    = var.project_id
+  location   = "EU"
+
+}
+
+resource "google_bigquery_dataset" "ads_dataset" {
+  dataset_id = "instaspy_ads_dataset"
+  project    = var.project_id
+  location   = "EU"
+
 }
 
