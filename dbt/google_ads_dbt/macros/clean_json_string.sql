b480a55de452f011e59eb1d48bba99033ff42b15
d0ndada <pintoavila09@gmail.com>
2024-10-07 21:10:35 +0200

    perf: usage of macros in clean CTE

---
+++ dbt/google_ads_dbt/macros/clean_json_string.sql
@@ -0,0 +1,7 @@
+{% macro clean_json_string(json_field) %}
+    REGEXP_REPLACE(
+        REGEXP_REPLACE(JSON_EXTRACT({{ json_field }}, '$.region_stats_json'), '^"+|"+$', ''),
+        '\\\\',
+        ''
+    )
+{% endmacro %}
