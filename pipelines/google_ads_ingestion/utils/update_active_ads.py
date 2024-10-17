46de3fe34412e3fea56c9bd947fb01aca53157e9
d0ndada <pintoavila09@gmail.com>
2024-10-17 14:01:22 +0200

    feat: update active ads based on clean_google_ads column is_active

---
+++ pipelines/google_ads_ingestion/utils/update_active_ads.py
@@ -0,0 +1,74 @@
+import logging
+from google.cloud import bigquery
+
+
+def update_active_ads(
+    bigquery_client: bigquery.Client,
+    project_id: str,
+    dataset_id: str,
+    raw_table_id: str,
+    staging_table_id: str,  
+) -> None:
+    """
+    Update existing Google Ads data for active ads flagged in the staging table.
+
+    Args:
+    bigquery_client (bigquery.Client): BigQuery client instance.
+    project_id (str): Google Cloud project ID.
+    dataset_id (str): BigQuery dataset ID.
+    raw_table_id (str): Raw table ID to update.
+    staging_table_id (str): Temporary staging table ID containing active creative_ids.
+
+    Raises:
+    Exception: If the query execution or data update fails.
+    """
+    query = f"""
+    INSERT INTO `{project_id}.{dataset_id}.{raw_table_id}` (advertiser_id, creative_id, raw_data, metadata_time, data_modified)
+    SELECT 
+        t.advertiser_id,
+        t.creative_id,
+        PARSE_DATE('%Y-%m-%d', region.first_shown) AS data_modified
+        CURRENT_TIMESTAMP() AS metadata_time,
+        TO_JSON_STRING(STRUCT(
+        t.advertiser_id,
+        t.creative_id,
+        t.creative_page_url,
+        t.ad_format_type,
+        t.advertiser_disclosed_name,
+        t.advertiser_legal_name,
+        t.advertiser_location,
+        t.advertiser_verification_status,
+        t.topic,
+        t.is_funded_by_google_ad_grants,
+        ARRAY(
+            SELECT AS STRUCT *
+            FROM UNNEST(t.region_stats) AS region
+            WHERE region.region_code = "SE"
+        ) AS region_stats
+    )) AS raw_data,
+    FROM
+        `bigquery-public-data.google_ads_transparency_center.creative_stats` AS t,
+        UNNEST(t.region_stats) AS region
+    WHERE t.creative_id IN (SELECT creative_id FROM `{project_id}.{dataset_id}.{staging_table_id}`)
+    AND NOT EXISTS (
+        SELECT 1 
+        FROM `{project_id}.{dataset_id}.{raw_table_id}` AS existing
+        WHERE existing.advertiser_id = t.advertiser_id
+        AND existing.creative_id = t.creative_id
+        AND existing.raw_data = TO_JSON_STRING(t)
+    )
+    """
+    
+    try:
+        query_job = bigquery_client.query(query)
+        query_job.result()
+        bigquery_client.query(
+                f"DROP TABLE IF EXISTS `{project_id}.{dataset_id}.{staging_table_id}`"
+            ).result()
+        logging.info(f"Existing data updated for active ads in staging table")
+        print(f"Existing data updated for active ads in staging table")
+
+    except Exception as e:
+        logging.error(f"Failed to update existing ads: {e}")
+        print(f"Failed to update existing ads: {e}")
+        raise
