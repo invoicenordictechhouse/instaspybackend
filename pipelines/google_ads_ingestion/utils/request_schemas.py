39c1bc88b1de554beba420d65aa74afe7e45e562
d0ndada <pintoavila09@gmail.com>
2024-10-17 14:12:25 +0200

    feat: request schemas for endpoints

---
+++ pipelines/google_ads_ingestion/utils/request_schemas.py
@@ -0,0 +1,14 @@
+from typing import List
+from utils.updateEnum import UpdateMode
+
+from pydantic import BaseModel, Field
+
+
+class BackfillRequest(BaseModel):
+    start_date: str = Field(..., pattern=r'^\d{4}-\d{2}-\d{2}$', description="Start date in 'YYYY-MM-DD' format")
+    end_date: str = Field(..., pattern=r'^\d{4}-\d{2}-\d{2}$', description="End date in 'YYYY-MM-DD' format")
+    advertiser_ids: List[str] = Field(..., min_items=1, description="List of advertiser_id_secondary for backfill")
+
+class UpdateRequest(BaseModel):
+    update_mode: UpdateMode
+    advertiser_ids: List[str] = None
