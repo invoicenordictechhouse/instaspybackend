1a0ab23487bcabe44045d36f346923a5afe52a82
d0ndada <pintoavila09@gmail.com>
2024-10-17 14:10:08 +0200

    perf: improving logic to work with new insert google ads daily

--- pipelines/google_ads_ingestion/utils/fetch_google_ads_data.py
+++ pipelines/google_ads_ingestion/utils/fetch_google_ads_data.py
@@ -1,20 +1,19 @@
-from datetime import datetime, timedelta
 import logging
-from pytz import timezone
-from .bigquery_client import bigquery_client
-from .insert_incremental_google_ads_data import insert_incremental_google_ads_data
 
-# Stockholm timezone
-STOCKHOLM_TZ = timezone("Europe/Stockholm")
+from .update_existing_google_ads_data import update_existing_google_ads_data
+from .store_active_creative_ids_in_staging import store_active_creative_ids_in_staging
+from .bigquery_client import bigquery_client
+from .insert_new_google_ads_data import insert_new_google_ads_data
 
 
 def fetch_google_ads_data(
     dataset_id: str,
     project_id: str,
     table_id: str,
-    backfill=False,
-    start_date=None,
-    end_date=None,
+    backfill:bool = False,
+    start_date: str =None,
+    end_date:str = None,
+    advertiser_ids: list = None,
 ) -> None:
     """
     Fetch Google Ads data from BigQuery and insert it incrementally.
@@ -34,21 +33,17 @@ def fetch_google_ads_data(
     ValueError: If `backfill` is True and `start_date` or `end_date` is not provided.
     """
     try:
-        if not backfill:
-            stockholm_time = datetime.now(STOCKHOLM_TZ)
-            start_date = (stockholm_time - timedelta(days=1)).strftime("%Y-%m-%d")
-            end_date = stockholm_time.strftime("%Y-%m-%d")
-
-            logging.info(f"Fetching previous day's data: {start_date} to {end_date}")
-        else:
+        if backfill:
             if not start_date or not end_date:
-                raise ValueError(
-                    "Please provide both start_date and end_date for backfill."
-                )
-            logging.info(f"Running backfill from {start_date} to {end_date}")
-
-        insert_incremental_google_ads_data(
-            bigquery_client, project_id, dataset_id, table_id, start_date, end_date
-        )
+                raise ValueError("Both start_date and end_date must be provided for backfill.")
+            logging.info(f"Backfilling data from {start_date} to {end_date}")
+            insert_new_google_ads_data(
+                bigquery_client=bigquery_client, project_id=project_id, dataset_id=dataset_id, table_id=table_id, backfill=backfill, start_date=start_date, end_date=end_date, advertiser_ids=advertiser_ids
+            )
+        else:
+            insert_new_google_ads_data(
+                bigquery_client=bigquery_client, project_id=project_id, dataset_id=dataset_id, table_id=table_id,backfill=backfill, start_date=start_date, end_date=end_date
+            )
+            
     except Exception as e:
         logging.error(f"Failed to fetch Google Ads data: {e}")
