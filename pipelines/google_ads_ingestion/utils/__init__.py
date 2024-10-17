6e5a3be48c859b3814bae38711d8d6ba7f7537de
d0ndada <pintoavila09@gmail.com>
2024-10-17 14:13:16 +0200

    chore: update imports

--- pipelines/google_ads_ingestion/utils/__init__.py
+++ pipelines/google_ads_ingestion/utils/__init__.py
@@ -3,6 +3,18 @@ from .create_incremental_table_if_not_exists import (
     create_incremental_table_if_not_exists as create_incremental_table_if_not_exists,
 )
 from .fetch_google_ads_data import fetch_google_ads_data as fetch_google_ads_data
-from .insert_incremental_google_ads_data import (
-    insert_incremental_google_ads_data as insert_incremental_google_ads_data,
+from .insert_new_google_ads_data import (
+    insert_new_google_ads_data as insert_new_google_ads_data,
 )
+
+from .store_active_creative_ids_in_staging import store_active_creative_ids_in_staging as store_active_creative_ids_in_staging 
+
+from .update_existing_google_ads_data import update_existing_google_ads_data as update_existing_google_ads_data
+
+from .updateEnum import UpdateMode as UpdateMode
+
+from .request_schemas import (
+    UpdateRequest as UpdateRequest,
+    BackfillRequest as BackfillRequest
+)
+
