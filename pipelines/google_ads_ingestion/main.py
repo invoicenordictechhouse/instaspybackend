f5e650b4656f970148b0d5131ea1d54a66a155c1
d0ndada <pintoavila09@gmail.com>
2024-10-28 21:48:34 +0100

    ref: clean up logic

--- pipelines/google_ads_ingestion/main.py
+++ pipelines/google_ads_ingestion/main.py
@@ -1,23 +1,10 @@
 import uvicorn
-from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from routers import ads, health, ingestion
 
-project_id = "annular-net-436607-t0"
-dataset_id = "sample_ds"
-table_id = "raw_sample_ads"
-staging_table_id = "staging_active_sample_ids"
-clean_table_id = "clean_google_ads"
+app = FastAPI()
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    print("Starting Google Ads Data Ingestion API...")
-    yield
-    print("Shutting down Google Ads Data Ingestion API...")
-
-app = FastAPI(lifespan=lifespan)
-
-app.include_router(ads.router, prefix="/ingestion/ads", tags=["Ads Update"])
+app.include_router(ads.router, prefix="/ads", tags=["Ads Update"])
 app.include_router(ingestion.router, prefix="/ingestion", tags=["Ingestion"])
 app.include_router(health.router, prefix="/health", tags=["Health"])
 
