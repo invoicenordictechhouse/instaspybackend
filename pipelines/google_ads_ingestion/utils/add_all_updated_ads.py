13035ad0e31faf70475803ebab743b41e174e5dd
d0ndada <pintoavila09@gmail.com>
2024-10-31 09:36:24 +0100

    perf: error handling when no got updated

--- pipelines/google_ads_ingestion/utils/add_all_updated_ads.py
+++ pipelines/google_ads_ingestion/utils/add_all_updated_ads.py
@@ -1,5 +1,8 @@
 from config import ADVERTISERS_TRACKING_TABLE_ID
 from google.cloud import bigquery
+from enums.IngestionStatus import IngestionStatus
+
+from utils.check_table_row_count import check_table_row_count
 
 
 def add_all_updated_ads(
@@ -7,19 +10,28 @@ def add_all_updated_ads(
     project_id: str,
     dataset_id: str,
     raw_table_id: str,
-) -> None:
+) -> IngestionStatus:
     """
-    Adds new versions of ads for all advertisers listed in the ADVERTISER_IDS_TABLE.
+    Inserts updated ad versions for all advertisers listed in the ADVERTISERS_TRACKING_TABLE_ID.
 
-    This function inserts new ad records if they have been updated or modified, ensuring only unique
-    records are added to retain all ad versions over time.
+    This function retrieves the latest ad records for advertisers in the specified tracking table, 
+    and inserts new or modified ad records into the target raw table, ensuring only unique 
+    records are added based on raw data changes. This maintains historical versions of each ad.
 
     Args:
-    bigquery_client (bigquery.Client): BigQuery client instance.
-    project_id (str): Google Cloud project ID.
-    dataset_id (str): BigQuery dataset ID.
-    raw_table_id (str): Raw table ID to update.
+        bigquery_client (bigquery.Client): An instance of BigQuery client to execute queries.
+        project_id (str): The Google Cloud project ID where BigQuery datasets reside.
+        dataset_id (str): The ID of the dataset containing both the target and tracking tables.
+        raw_table_id (str): The ID of the raw table where updated ad data is stored.
+
+    Returns:
+        IngestionStatus: An enum value indicating the status of the insertion process:
+            - DATA_INSERTED: New rows were successfully added to the raw table.
+            - NO_NEW_UPDATES: No new rows were added, as all ads already existed in the table.
+
     """
+    initial_row_count = check_table_row_count(bigquery_client, project_id, dataset_id, raw_table_id)
+
     query = f"""
     INSERT INTO `{project_id}.{dataset_id}.{raw_table_id}` 
     (data_modified, metadata_time, advertiser_id, creative_id, raw_data)
@@ -68,10 +80,16 @@ def add_all_updated_ads(
     WHERE NOT EXISTS (
         SELECT 1
         FROM `{project_id}.{dataset_id}.{raw_table_id}` AS existing
-        WHERE existing.advertiser_id = filtered_ads.advertiser_id
-        AND existing.creative_id = filtered_ads.creative_id
-        AND existing.raw_data = filtered_ads.raw_data
+        WHERE existing.raw_data = filtered_ads.raw_data
     )
     """
     query_job = bigquery_client.query(query)
     query_job.result()
+
+    final_row_count = check_table_row_count(bigquery_client, project_id, dataset_id, raw_table_id)
+
+    if final_row_count > initial_row_count:
+        return IngestionStatus.DATA_INSERTED
+    else:
+        return IngestionStatus.NO_NEW_UPDATES
+
