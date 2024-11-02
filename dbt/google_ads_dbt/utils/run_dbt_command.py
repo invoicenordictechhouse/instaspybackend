e56a130b72942df94a2e210ac5a5963aba89bfb1
d0ndada <pintoavila09@gmail.com>
2024-11-02 21:00:01 +0100

    perf:  catching errors better

--- dbt/google_ads_dbt/utils/run_dbt_command.py
+++ dbt/google_ads_dbt/utils/run_dbt_command.py
@@ -1,6 +1,8 @@
 import subprocess
 import logging
 
+from fastapi import HTTPException
+
 
 def run_dbt_command(command: str):
     """
@@ -10,7 +12,17 @@ def run_dbt_command(command: str):
         result = subprocess.run(
             command, shell=True, check=True, capture_output=True, text=True
         )
-        logging.info(f"DBT command executed successfully: {result.stdout}")
+        if result.returncode != 0:
+            error_message = result.stderr.strip()
+            logging.error(f"DBT command failed with error: {error_message}")
+            
+            raise HTTPException(status_code=500, detail=f"DBT command failed: {error_message}")
+
+        logging.info(f"DBT command - {result.stdout} completed successfully.")
+
     except subprocess.CalledProcessError as e:
-        logging.error(f"DBT command failed: {e.stderr}")
-        raise
+        logging.error(f"Failed to execute dbt command: {e}")
+        raise HTTPException(status_code=500, detail="An unexpected error occurred while executing the dbt command.")
+    except Exception as e:
+        logging.error(f"Unexpected error: {e}")
+        raise HTTPException(status_code=500, detail="An unexpected error occurred.")
