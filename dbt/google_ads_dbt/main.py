7077a8d1fc77eb25a971fbab15d307a91d56e262
d0ndada <pintoavila09@gmail.com>
2024-11-02 21:11:33 +0100

    perf: using healt route and new conusme prep

--- dbt/google_ads_dbt/main.py
+++ dbt/google_ads_dbt/main.py
@@ -1,11 +1,13 @@
-from routers import health, clean
+from routers import clean, health, consume_prep
 import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
-app.include_router(health.router, prefix="/health", tags=["Health Check"])
+app.include_router(health.router, tags=["Health Check"])
 app.include_router(clean.router, prefix="/clean", tags=["Cleaning"])
+app.include_router(consume_prep.router, prefix="/consume-prep", tags=["Consume-prep"])
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
