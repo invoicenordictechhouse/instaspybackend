eaa211e5ac8acd9aff79fffc18ff6b94bedbbe05
d0ndada <pintoavila09@gmail.com>
2024-11-02 21:04:22 +0100

    perf: usage of model for advertisers and for swagger

--- dbt/google_ads_dbt/routers/clean.py
+++ dbt/google_ads_dbt/routers/clean.py
@@ -1,14 +1,20 @@
 import logging
-from typing import List
 from fastapi import APIRouter, BackgroundTasks, HTTPException
+from schemas.AdvertiserRequest import AdvertiserRequest
 from utils.run_dbt_command import run_dbt_command
 
 router = APIRouter()
 
+
 @router.post(
     "/all",
     summary="Clean All Advertisers",
-    description="Trigger DBT model for all advertisers",
+    description="Trigger DBT model Clean for all advertisers",
+    responses={
+        200: {"description": "Cleaning initiated successfully", "content": {"application/json": {"example": {"status": "Cleaning initiated for all advertisers (NO_ID)"}}}},
+        400: {"description": "Bad Request - No advertiser_ids provided", "content": {"application/json": {"example": {"detail": "No advertiser_ids provided"}}}},
+        500: {"description": "Internal Server Error", "content": {"application/json": {"example": {"detail": "An unexpected error occurred"}}}},
+    }
 )
 async def clean_all_advertisers(background_tasks: BackgroundTasks):
     """
@@ -19,36 +25,49 @@ async def clean_all_advertisers(background_tasks: BackgroundTasks):
             run_dbt_command, "dbt run --select clean --vars '{advertiser_id: NO_ID}'"
         )
         return {"status": "Cleaning initiated for all advertisers (NO_ID)"}
+    except HTTPException as http_exc:
+        raise http_exc
     except Exception as e:
         logging.error(f"Failed to initiate cleaning: {e}")
-        raise HTTPException(status_code=500, detail="Cleaning failed")
+        raise HTTPException(status_code=500, detail="An unexpected error occurred.")
+
 
 @router.post(
     "/specific",
     summary="Clean Specific Advertisers",
-    description="Trigger DBT model for specific advertisers by IDs",
+    description="Trigger DBT model Clean for specific advertisers by IDs",
+    responses={
+        200: {"description": "Cleaning initiated successfully", "content": {"application/json": {"example": {"status": "Cleaning initiated for advertisers ['12345', '67890']"}}}},
+        400: {"description": "Bad Request - No advertiser_ids provided", "content": {"application/json": {"example": {"detail": "No advertiser_ids provided"}}}},
+        500: {"description": "Internal Server Error", "content": {"application/json": {"example": {"detail": "An unexpected error occurred"}}}},
+    }
 )
 async def clean_specific_advertiser(
-    advertiser_ids: List[str], background_tasks: BackgroundTasks
+    request: AdvertiserRequest, background_tasks: BackgroundTasks
 ):
     """
     Endpoint to trigger DBT model that cleans data for a specific advertiser.
+
+    - **advertiser_ids**: Accepts a single advertiser ID as a string or multiple IDs as a list.
     """
-    if not advertiser_ids:
-        raise HTTPException(status_code=400, detail="No advertiser_ids provided")
+    advertiser_ids = request.advertiser_ids
+    if isinstance(advertiser_ids, str):
+        ids_str = advertiser_ids
+    elif isinstance(advertiser_ids, list) and advertiser_ids:
+        ids_str = ",".join(advertiser_ids)
+    else:
+        raise HTTPException(status_code=400, detail="No valid advertiser_ids provided")
 
     try:
-        ids_str = ",".join(advertiser_ids)
         background_tasks.add_task(
             run_dbt_command,
             f"dbt run --select clean --vars '{{advertiser_ids: [{ids_str}]}}'",
         )
         return {"status": f"Cleaning initiated for advertisers {advertiser_ids}"}
+    except HTTPException as http_exc:
+        raise http_exc
     except Exception as e:
-        logging.error(
-            f"Failed to initiate cleaning for advertisers {advertiser_ids}: {e}"
-        )
-        raise HTTPException(
-            status_code=500, detail="Cleaning failed for the specified advertisers"
-        )
+        logging.error(f"Failed to initiate cleaning on advertiser {advertiser_ids}: {e}")
+        raise HTTPException(status_code=500, detail="An unexpected error occurred.")
+
 
