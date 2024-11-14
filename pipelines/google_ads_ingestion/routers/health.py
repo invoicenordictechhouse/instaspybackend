2c80fec5e92c75167c72e7975f8013549a096917
d0ndada <pintoavila09@gmail.com>
2024-11-14 13:44:53 +0100

    ref: html code seperated

--- pipelines/google_ads_ingestion/routers/health.py
+++ pipelines/google_ads_ingestion/routers/health.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
+templates = Jinja2Templates(directory="templates")
 
 
 @router.get(
@@ -10,48 +12,8 @@ router = APIRouter()
     response_class=HTMLResponse,
     include_in_schema=False,
 )
-async def root():
+async def root(request: Request):
     """
     Root endpoint providing a centered welcome message with a clickable link to the API documentation.
     """
-    return """
-    <html>
-        <head>
-            <style>
-                body {
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    height: 100vh;
-                    font-family: Arial, sans-serif;
-                    background-color: #f4f4f9;
-                    margin: 0;
-                }
-                .container {
-                    text-align: center;
-                    padding: 20px;
-                    background-color: white;
-                    border-radius: 10px;
-                    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-                }
-                h2 {
-                    color: #333;
-                }
-                a {
-                    text-decoration: none;
-                    color: #007bff;
-                    font-weight: bold;
-                }
-                a:hover {
-                    color: #0056b3;
-                }
-            </style>
-        </head>
-        <body>
-            <div class="container">
-                <h2>Welcome to the Google Ads Ingestion</h2>
-                <p><a href="/docs">Click here to view the API documentation</a></p>
-            </div>
-        </body>
-    </html>
-    """
+    return templates.TemplateResponse("welcome.html", {"request": request})
